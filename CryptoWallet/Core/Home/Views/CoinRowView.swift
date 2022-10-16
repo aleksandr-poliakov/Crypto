eab6f5fc808a61ae1b78155b2df10f296aef2515
alexander.poliakov <alexander.poliakov96@gmail.com>
2022-10-16 15:25:42 +0300

    Implemented coin row view

--- CryptoWallet/Core/Home/Views/CoinRowView.swift
+++ CryptoWallet/Core/Home/Views/CoinRowView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct CoinRowView: View {
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(6)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                Text("\(coin.priceChangePercentage24H)%")
+                    .foregroundColor(coin.priceChangePercentage24H >= 0 ? Color.theme.green : Color.theme.red)
+            }
+        }
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView()
+        CoinRowView(coin: dev.coin)
     }
 }
