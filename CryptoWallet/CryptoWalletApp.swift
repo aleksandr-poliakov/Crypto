7fa12d26046ac1a1ea5053b8fe4fde29f3959b8c
alexander.poliakov <alexander.poliakov96@gmail.com>
2022-10-16 13:30:18 +0300

    Initial Commit

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Aleksandr on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
